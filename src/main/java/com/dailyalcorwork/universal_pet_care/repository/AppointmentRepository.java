daa1d8dc81146d4af6e542126caf73ca086d2aac
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-27 03:52:36 +0300

    Appointment repository interface created and extended with jparepository

---
+++ src/main/java/com/dailyalcorwork/universal_pet_care/repository/AppointmentRepository.java
@@ -0,0 +1,7 @@
+package com.dailyalcorwork.universal_pet_care.repository;
+
+import com.dailyalcorwork.universal_pet_care.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
