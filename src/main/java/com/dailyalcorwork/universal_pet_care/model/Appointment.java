7fdc0b2f1fa59233a0c539e9e73f9748723a05a7
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-27 03:11:46 +0300

    appointment class resetted

--- src/main/java/com/dailyalcorwork/universal_pet_care/model/Appointment.java
+++ src/main/java/com/dailyalcorwork/universal_pet_care/model/Appointment.java
@@ -1,15 +1,58 @@
 package com.dailyalcorwork.universal_pet_care.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Random;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String reason;
     private LocalDate date;
     private LocalTime time;
     private String appointmentNo;
     private LocalDate createdAt;
+
+    @JoinColumn(name = "sender")
+    @ManyToOne(fetch = FetchType.LAZY)
     private User patient;
+
+    @Column(name = "recipient")
+    @ManyToOne(fetch = FetchType.LAZY)
     private User veterinarian;
+
+    public void addPatient(User sender) {
+        this.setPatient(sender);
+        if (sender.getAppointments() == null) {
+            sender.setAppointments(new ArrayList<>());
+        }
+        sender.getAppointments().add(this);
+    }
+
+    // veterinarian için
+    public void addVeterinarian(User recipient) {
+        this.setVeterinarian(recipient);
+        if (recipient.getAppointments() == null) {
+            recipient.setAppointments(new ArrayList<>());
+        }
+        recipient.getAppointments().add(this);
+    }
+
+    // appointmentNo için random bir string value olusturacak fonksiyon
+    public void setAppointmentNo(String appointmentNo) {
+        this.appointmentNo = String.valueOf(new Random().nextLong()).substring(1, 11);
+    }
 }
