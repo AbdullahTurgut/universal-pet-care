b9ddaab382db6e07a6468825c4973d8642be4e66
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-03-29 02:15:04 +0300

    controller created for pet table

---
+++ src/main/java/com/dailyalcorwork/universal_pet_care/controller/PetController.java
@@ -0,0 +1,74 @@
+package com.dailyalcorwork.universal_pet_care.controller;
+
+
+import com.dailyalcorwork.universal_pet_care.exception.ResourceNotFoundException;
+import com.dailyalcorwork.universal_pet_care.model.Pet;
+import com.dailyalcorwork.universal_pet_care.response.ApiResponse;
+import com.dailyalcorwork.universal_pet_care.service.pet.IPetService;
+import com.dailyalcorwork.universal_pet_care.utils.FeedBackMessage;
+import com.dailyalcorwork.universal_pet_care.utils.UrlMapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(UrlMapping.PETS)
+public class PetController {
+
+    private final IPetService petService;
+
+    @GetMapping(UrlMapping.SAVE_PETS_FOR_APPOINTMENT)
+    public ResponseEntity<ApiResponse> savePets(@RequestBody List<Pet> pets) {
+        try {
+            List<Pet> savedPets = petService.savePetForAppointment(pets);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.SUCCESS, savedPets));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    @GetMapping(UrlMapping.GET_PET_BY_ID)
+    public ResponseEntity<ApiResponse> getPetsById(@PathVariable Long petId) {
+        try {
+            Pet pet = petService.getPetById(petId);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.FOUND, pet));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+
+    @DeleteMapping(UrlMapping.DELETE_PET_BY_ID)
+    public ResponseEntity<ApiResponse> deletePetById(@PathVariable Long petId) {
+        try {
+            petService.deletePet(petId);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.DELETE_SUCCESS, null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping(UrlMapping.UPDATE_PET)
+    public ResponseEntity<ApiResponse> updatePet(@PathVariable Long petId, @RequestBody Pet pet) {
+        try {
+            Pet updatedPet = petService.updatePet(petId, pet);
+            return ResponseEntity.ok(new ApiResponse(FeedBackMessage.UPDATE_SUCCESS, updatedPet));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
+}
--- src/main/java/com/dailyalcorwork/universal_pet_care/utils/UrlMapping.java
+++ src/main/java/com/dailyalcorwork/universal_pet_care/utils/UrlMapping.java
@@ -10,6 +10,7 @@ public class UrlMapping {
     public static final String GET_ALL_USERS = "/all-users";
 
     /*======================= START APPOINTMENT API =================================*/
+    
     public static final String APPOINTMENT = API + "/appointments";
     public static final String BOOK_APPOINTMENT = "/book-appointment";
     public static final String GET_APPOINTMENT_BY_ID = "/{id}/appointment";
@@ -17,6 +18,18 @@ public class UrlMapping {
     public static final String GET_ALL_APPOINTMENTS = "/all";
     public static final String UPDATE_APPOINTMENT = "/{id}/update";
     public static final String DELETE_APPOINTMENT_BY_ID = "/{id}/delete";
+
     /*======================= END APPOINTMENT API =================================*/
+
+
+    /*====================== START PET API ==========================================*/
+
+    public static final String PETS = API + "/pets";
+    public static final String SAVE_PETS_FOR_APPOINTMENT = "/all";
+    public static final String GET_PET_BY_ID = "/{id}/pet";
+    public static final String DELETE_PET_BY_ID = "/{id}/delete";
+    public static final String UPDATE_PET = "/{id}/update";
+
+    /*====================== END PET API ==========================================*/
 
 }
