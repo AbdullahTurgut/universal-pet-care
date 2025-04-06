52d05271361b4696d722f1611e118334675949f6
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-04-06 23:43:02 +0300

    patientdto and veterinariandto classes created for existing details of users that which we want to show

---
+++ src/main/java/com/dailyalcorwork/universal_pet_care/dto/PatientDto.java
@@ -0,0 +1,13 @@
+package com.dailyalcorwork.universal_pet_care.dto;
+
+import lombok.Data;
+
+@Data
+public class PatientDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String phoneNumber;
+}
