a8c832979472a6f5f52f339cfe472bf1c32ffc61
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-04-15 02:22:18 +0300

    created a jsx file for login page

---
+++ frontend/universal-pet-care/src/components/auth/Login.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
+import { BsPersonFill, BsLockFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [credentials, setCredentials] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  return (
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col sm={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title className="text-center mb-4"></Card.Title>
+              <Form>
+                <Form.Group className="mb-3" controlId="username">
+                  <Form.Label>Username</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <BsPersonFill />
+                      {/* Icon for username */}"
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="text"
+                      name="email"
+                      value={credentials.email}
+                      onChange={handleInputChange}
+                    />
+                  </InputGroup>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                  <Form.Label>Password</Form.Label>
+                  <InputGroup>
+                    <InputGroup.Text>
+                      <BsLockFill />
+                      {/* Icon for password */}
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      value={credentials.password}
+                      onChange={handleInputChange}
+                    />
+                  </InputGroup>
+                </Form.Group>
+                <Button
+                  variant="outline-primary"
+                  type="submit"
+                  className="w-100"
+                >
+                  Login
+                </Button>
+              </Form>
+              <div className="text-center mt-2">
+                Don't have an account yet?{" "}
+              </div>
+              <Link
+                to={"/user-registration"}
+                style={{ textDecoration: "none" }}
+              >
+                Register here
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
