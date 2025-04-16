86ae54c6a1c384b26d353aa2eed8982e44fbec80
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-04-17 02:08:35 +0300

    delete confirmation modal created for vet or patient can be delete their account actually.

---
+++ frontend/universal-pet-care/src/components/modals/DeleteConfirmationModal.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import ProcessSpinner from "../common/ProcessSpinner";
+
+const DeleteConfirmationModal = ({
+  show,
+  onHide,
+  onConfirm,
+  itemToDelete,
+  loading,
+}) => {
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>Delete Confirmation</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Are you sure you want to delete {itemToDelete}? This action can not be
+        undone.
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Cancel
+        </Button>
+
+        <Button variant="danger" onClick={onConfirm}>
+          Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default DeleteConfirmationModal;
