4c72fae94914628cdf8453bfafb55b94589f3313
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-04-18 03:55:52 +0300

    assignment has done. veterinarian actions coded.

--- frontend/universal-pet-care/src/components/actions/VeterinarianActions.jsx
+++ frontend/universal-pet-care/src/components/actions/VeterinarianActions.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import ActionButtons from "./ActionButtons";
 
-const VeterinarianActions = () => {
-  return <div>VeterinarianActions</div>;
+const VeterinarianActions = ({ onApprove, onDecline, isDisabled }) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleActionClick = (actionType) => {
+    setIsProcessing(true);
+    if (actionType === "Approve") {
+      onApprove()
+        .then(() => {
+          setIsProcessing(false);
+        })
+        .catch(() => {
+          setIsProcessing(false);
+        });
+    } else {
+      onDecline()
+        .then(() => {
+          setIsProcessing(false);
+        })
+        .catch(() => {
+          setIsProcessing(false);
+        });
+    }
+  };
+
+  return (
+    <section>
+      <ActionButtons
+        title={"Decline Appointment"}
+        variant={"danger"}
+        onClick={() => handleActionClick("Decline")}
+        disabled={isDisabled}
+        isProcessing={isProcessing}
+      />
+      <ActionButtons
+        title={"Approve Appointment"}
+        variant={"danger"}
+        onClick={() => handleActionClick("Approve")}
+        disabled={isDisabled}
+        isProcessing={isProcessing}
+      />
+    </section>
+  );
 };
 
 export default VeterinarianActions;
