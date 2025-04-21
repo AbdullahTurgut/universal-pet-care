d27b5233ec26dda7e13799f2d2f2d29554b32d11
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-04-22 01:38:22 +0300

    created a file for admin dashboard sidebars

--- frontend/universal-pet-care/src/components/admin/AdminDashboard.jsx
+++ frontend/universal-pet-care/src/components/admin/AdminDashboard.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { AdminOverview } from "./AdminOverview";
+import { AdminDashboardSidebar } from "./AdminDashboardSidebar";
 
 export const AdminDashboard = () => {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+  const [activeContent, setActiveContent] = useState("overview");
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+  const handleNavigate = (component) => {
+    setActiveContent(component);
+  };
   return (
-    <div>
-      <AdminOverview />
-    </div>
+    <main className="admin-body">
+      <div className="grid-container">
+        <AdminDashboardSidebar
+          openSidebarToggle={openSidebarToggle}
+          OpenSidebar={OpenSidebar}
+          onNavigate={handleNavigate}
+          activeTab={activeContent}
+        />
+        <div className="main-container">
+          {activeContent === "overview" && <AdminOverview />}
+        </div>
+      </div>
+    </main>
   );
 };
---
+++ frontend/universal-pet-care/src/components/admin/AdminDashboardSidebar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  BsFillHospitalFill,
+  BsGrid1X2Fill,
+  BsPeopleFill,
+  BsX,
+} from "react-icons/bs";
+
+export const AdminDashboardSidebar = ({
+  openSidebarToggle,
+  OpenSidebar,
+  onNavigate,
+  activeTab,
+}) => {
+  return (
+    <aside
+      id="sidebar"
+      className={openSidebarToggle ? "sidebar-responsive" : ""}
+    >
+      <div className="sidebar-title">
+        <div className="sidebar-brand">
+          <BsFillHospitalFill className="icon-header" /> UniPet Care
+        </div>
+        <span className="icon icon-close" onClick={OpenSidebar}>
+          <BsX />
+        </span>
+      </div>
+      <ul className="sidebar-list">
+        <li
+          className={`sidebar-list-item ${
+            activeTab === "overview" ? "active" : ""
+          }`}
+          onClick={() => onNavigate("overview")}
+        >
+          <a href="#">
+            <BsGrid1X2Fill className="icon" /> Dashboard Overview
+          </a>
+        </li>
+        <li
+          className={`sidebar-list-item ${
+            activeTab === "veterinarians" ? "active" : ""
+          }`}
+          onClick={() => onNavigate("veterinarians")}
+        >
+          <a href="#">
+            <BsPeopleFill className="icon" /> Veterinarians
+          </a>
+        </li>
+        <li
+          className={`sidebar-list-item ${
+            activeTab === "patients" ? "active" : ""
+          }`}
+          onClick={() => onNavigate("patients")}
+        >
+          <a href="#">
+            <BsPeopleFill className="icon" /> Patients
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+};
